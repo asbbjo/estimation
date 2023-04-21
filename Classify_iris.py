39abc5583cc35e88600e9bab867fb9d47866f7b7
vebroo <vebjorn.rooth@gmail.com>
2023-04-21 18:09:07 +0200

    made task 1 into self defined functions

--- Classify_iris.py
+++ Classify_iris.py
@@ -2,12 +2,113 @@ import numpy as np
 import scipy.special as sc
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-np.random.seed(50)
+np.random.seed(100)
+
+##########----------                    ----------##########
+##########---------- DEFINING FUNCTIONS ----------##########
+##########----------                    ----------##########
+
+##########---------- FOR TASK 1 ----------##########
+
+# read data from the 3 different flowers
+def read_data(filename: str):
+    data = np.loadtxt(filename, delimiter=",", usecols=(0, 1, 2, 3))
+    setosa = data[0:50]
+    versicolor = data[50:100]
+    virginica = data[100:150]
+    return setosa, versicolor, virginica, data
+
+# create data vector x
+# default: only appending 1 and not deleting any elements
+def make_x_data(setosa: np.ndarray, versicolor: np.ndarray, virginica: np.ndarray, delete_index=slice(0,0)):
+    setosa_x = np.array([np.append(np.delete(row, delete_index), 1) for row in setosa])
+    versicolor_x = np.array([np.append(np.delete(row, delete_index), 1) for row in versicolor])
+    virginica_x = np.array([np.append(np.delete(row, delete_index), 1) for row in virginica])
+    return setosa_x, versicolor_x, virginica_x
+
+# make training and test data
+# choose data set
+# first round: training uses the first 30, testing the last 20
+# second round: training uses the last 30, testing the first 20
+def make_training_and_test_data(setosa_x: np.ndarray, versicolor_x: np.ndarray, virginica_x: np.ndarray, data_set: int, training_num = 30, test_num = 20):
+    if (data_set == 1):
+        print('Using the data set of the FIRST round')
+        data_training = np.concatenate([setosa_x[:training_num], versicolor_x[:training_num], virginica_x[:training_num]])
+        data_test = np.concatenate([setosa_x[-test_num:], versicolor_x[-test_num:], virginica_x[-test_num:]])
+    elif (data_set == 2):
+        print('Using the data set of the SECOND round')
+        data_training = np.concatenate([setosa_x[-training_num:], versicolor_x[-training_num:], virginica_x[-training_num:]])
+        data_test = np.concatenate([setosa_x[:test_num], versicolor_x[:test_num], virginica_x[:test_num]])
+    else:
+        ValueError
+    return data_training, data_test
+
+def make_training_labels(training_num= 30):
+    t_training = np.zeros((3*training_num, 3, 1))
+    t_training[:training_num] = np.array([[1],[0],[0]])
+    t_training[training_num:2*training_num] = np.array([[0],[1],[0]])
+    t_training[2*training_num:] = np.array([[0],[0],[1]])
+    return t_training
+
+# train the classifier. needs dataset on the form [[float float ... float 1]
+#                                                  [float float ... float 1]
+#                                                  ...
+#                                                  [float float ... float 1]]
+# where the columns of each row is given by num_cols and number of rows is given by num_training (global variable)
+def train_classifier(alpha: float, tolerance: float, dataset: np.ndarray, num_cols_W: int, training_labels: np.ndarray, training_num=30):
+    W = np.zeros((3, num_cols_W))
+    W = np.random.randn(3, num_cols_W)
+    condition = True
+    num_iterations = 0
+    print('Using alpha=', alpha, ' and a tolerance=', tolerance, 'for the norm of grad_W_MSE')
+    while condition:
+        grad_W_MSE = 0  
+        for i in range(3*training_num):
+
+            xk_training = np.array([dataset[i]]).T
+            zk_training = np.dot(W, xk_training)
+            gk_training = sc.expit(zk_training)
+
+            grad_W_MSE += grad_W_MSE_k(gk_training, training_labels[i], xk_training)
+
+        W = W - alpha*grad_W_MSE
+        condition = np.linalg.norm(grad_W_MSE) >= tolerance
+        num_iterations += 1
+    print('Number of iterations to converge for the training set:', num_iterations)
+    return W
 
 # calculating the gradient of the matrix W times the MSE
 def grad_W_MSE_k(gk, tk, xk):
     return np.dot(np.multiply(gk - tk, gk, 1 - gk), xk.T)
 
+# test the classifier
+def test_classifier(W, dataset: np.ndarray, num: int):
+    g_predicted = []
+    for i in range(3*num):
+    
+        xk = np.array([dataset[i]]).T
+        zk = np.dot(W, xk)
+        gk = sc.expit(zk)
+
+        g_predicted.append(np.argmax(gk) + 1)
+    return g_predicted
+
+# calculate the confusion matrix
+def calculate_confusion_matrix(g_predicted: list, training_num= 30, test_num= 20):
+
+    if (len(g_predicted) == 3*training_num):
+        g_true = [1]*training_num + [2]*training_num + [3]*training_num
+    elif (len(g_predicted) == 3*test_num):
+        g_true = [1]*test_num + [2]*test_num + [3]*test_num
+    else:
+        ValueError
+    cm = confusion_matrix(g_true, g_predicted)
+
+    # normalize confusion matrix to percentages
+    cm_norm = np.round(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=3)
+    return cm, cm_norm
+
+# calculate error rate
 def error_rate(confusion_matrix):
     # Calculate the total number of predictions
     total_predictions = np.sum(confusion_matrix)
@@ -18,114 +119,39 @@ def error_rate(confusion_matrix):
     error_rate = round(error_rate, 3)
     return error_rate
 
-# read data from the 3 different flowers
-data = np.loadtxt("Classification Iris/Iris_TTT4275/iris.data", delimiter=",", usecols=(0, 1, 2, 3))
+def print_results(cm: np.ndarray, cm_norm: np.ndarray, string: str):
+    print('---', string.upper(),' SET:')
+    print('Confusion matrix:\n', cm, '\n\n', cm_norm)
+    print('\nError rate:', error_rate(cm))
+    print()
 
-setosa = data[0:50]
-versicolor = data[50:100]
-virginica = data[100:150]
+##########---------- FOR TASK 2 ----------##########
 
-# create vector x
-setosa_x = np.array([np.append(row, 1) for row in setosa])
-versicolor_x = np.array([np.append(row, 1) for row in versicolor])
-virginica_x = np.array([np.append(row, 1) for row in virginica])
 
-# read the data into a training and test set
-training_num = 30 
-test_num = 20
 
-# CHOOSE DATA SET FOR TRAINING AND TEST
-# first round: training uses the first 30, testing the last 20
-print('Using the data set of the FIRST round')
-data_training = np.concatenate([setosa_x[:training_num], versicolor_x[:training_num], virginica_x[:training_num]])
-data_test = np.concatenate([setosa_x[-test_num:], versicolor_x[-test_num:], virginica_x[-test_num:]])
+##########----------                    ----------##########
+##########----------   RUNNING TASK 1   ----------##########
+##########----------                    ----------##########
 
-# second round: training uses the last 30, testing the first 20
-# print('Using the data set of the SECOND round')
-# data_training = np.concatenate([setosa_x[-training_num:], versicolor_x[-training_num:], virginica_x[-training_num:]])
-# data_test = np.concatenate([setosa_x[:test_num], versicolor_x[:test_num], virginica_x[:test_num]])
 
-# create a vector with the correct corresponding labels 
-t_training = np.zeros((90, 3, 1))
-t_training[:30] = np.array([[1],[0],[0]])
-t_training[30:60] = np.array([[0],[1],[0]])
-t_training[60:] = np.array([[0],[0],[1]])
+setosa, versicolor, virginica, data = read_data("Classification Iris/Iris_TTT4275/iris.data")
+x_setosa, x_versicolor, x_virginica = make_x_data(setosa, versicolor, virginica)
+training_data, test_data = make_training_and_test_data(x_setosa, x_versicolor, x_virginica, data_set=1)
+t_labels = make_training_labels()
+W_matrix = train_classifier(alpha=0.001, tolerance=0.4, dataset=training_data, num_cols_W=5, training_labels=t_labels)
+g_pred_training = test_classifier(W=W_matrix, dataset=training_data, num=30)
+g_pred_test = test_classifier(W=W_matrix, dataset=test_data, num=20)
+cm_training, cm_norm_training = calculate_confusion_matrix(g_predicted=g_pred_training)
+cm_test, cm_norm_test = calculate_confusion_matrix(g_predicted=g_pred_test)
+print_results(cm=cm_training, cm_norm=cm_norm_training, string="training")
+print_results(cm=cm_test, cm_norm=cm_norm_test, string="test")
 
-# create matrix W (3,5) 
-# [w1a w1b w1c w1d w10]
-# [w2a w2b w2c w2d w20]
-# [w3a w3b w3c w3d w30]
 
-W = np.zeros((3, 5))
-W = np.random.randn(3, 5)
+##########----------                    ----------##########
+##########----------   RUNNING TASK 2   ----------##########
+##########----------                    ----------##########
 
-######----- train the classifier -----######
-# xk: (5,1), zk: (3,1), gk: (3,1)
-
-alpha = 0.001
-tolerance = 0.4
-condition = True
-num_iterations = 0
-print('Using alpha=', alpha, ' and a tolerance=', tolerance, 'for the norm of grad_W_MSE')
-while condition:
-    grad_W_MSE = 0  
-    for i in range(3*training_num):
-
-        xk_training = np.array([data_training[i]]).T
-        zk_training = np.dot(W, xk_training)
-        gk_training = sc.expit(zk_training)
-
-        grad_W_MSE += grad_W_MSE_k(gk_training, t_training[i], xk_training)
-
-    W = W - alpha*grad_W_MSE
-    condition = np.linalg.norm(grad_W_MSE) >= tolerance
-    num_iterations += 1
-print('Number of iterations to converge for the training set:', num_iterations)
-    
-######----- test the classifier -----######
-# using the training set
-g_predicted_training = []
-for i in range(3*training_num):
-    
-    xk_training = np.array([data_training[i]]).T
-    zk_training = np.dot(W, xk_training)
-    gk_training = sc.expit(zk_training)
-
-    g_predicted_training.append(np.argmax(gk_training) + 1)
-
-# using the test set
-g_predicted_test = []
-for i in range(3*test_num):
-    
-    xk_test = np.array([data_test[i]]).T
-    zk_test = np.dot(W, xk_test)
-    gk_test = sc.expit(zk_test)
-
-    g_predicted_test.append(np.argmax(gk_test) + 1)
-
-# calculate the confusion matrix for the training and test set
-g_true_training = [1]*training_num + [2]*training_num + [3]*training_num
-g_true_test = [1]*test_num + [2]*test_num + [3]*test_num
-
-cm_training = confusion_matrix(g_true_training, g_predicted_training)
-cm_test = confusion_matrix(g_true_test, g_predicted_test)
-
-# normalize confusion matrices to percentages
-cm_norm_training = np.round(cm_training.astype('float') / cm_training.sum(axis=1)[:, np.newaxis], decimals=3)
-cm_norm_test = np.round(cm_test.astype('float') / cm_test.sum(axis=1)[:, np.newaxis], decimals=3)
-
-print()
-print('---TRAINING SET:')
-print('Confusion matrix:\n', cm_training, '\n\n', cm_norm_training)
-print('\nError rate:', error_rate(cm_training))
-print('\n---TEST SET:')
-print('Confusion matrix:\n', cm_test, '\n\n', cm_norm_test)
-print('\nError rate:', error_rate(cm_test))
-
-##########---------- TASK 2 ----------##########
 # make histograms for each feture and class
-
-# create data sets for each feature and class
 
 sepal_length = [element[0] for element in data]
 sepal_width  = [element[1] for element in data]
@@ -151,6 +177,4 @@ for i in range(len(data_vector)):
     plt.suptitle('Histogram for all features and classes')
     plt.legend()
 plt.show()
-
-
 
