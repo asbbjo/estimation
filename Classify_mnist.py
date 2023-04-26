6234528936b874a53cf4de5816464bbfe35970e0
vebroo <vebjorn.rooth@gmail.com>
2023-04-26 11:38:33 +0200

    fixed filepaths

--- Classify_mnist.py
+++ Classify_mnist.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import struct
+import os
 
-fname = 'train_images.bin'
+filenames = ["train_images.bin", "train_labels.bin", "test_images.bin", "test_labels"]
+filepaths = []
+for i in range(4):
+    filepaths.append(os.path.join(os.getcwd(), "Classification MNIST\MNist_ttt4275", filenames[i]))
 
-with open(fname,'rb') as f:
+with open(filepaths[0],'rb') as f:
     magic, size = struct.unpack(">II", f.read(8))
     nrows, ncols = struct.unpack(">II", f.read(8))
     data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
     data = data.reshape((size, nrows, ncols))
 
-with open('train_labels.bin','rb') as f:
+with open(filepaths[1],'rb') as f:
     magic, size = struct.unpack(">II", f.read(8))
     data_num = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
     data_num = data_num.reshape((size,)) # (Optional)
