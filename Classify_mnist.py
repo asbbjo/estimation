bcfea569d51a2d05a86fc9bb11dddabb3753cb9b
vebroo <vebjorn.rooth@gmail.com>
2023-04-26 16:01:31 +0200

    read the data from the file

--- Classify_mnist.py
+++ Classify_mnist.py
@@ -2,24 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import struct
 import os
+import cProfile
+from sklearn.metrics import confusion_matrix
 
-filenames = ["train_images.bin", "train_labels.bin", "test_images.bin", "test_labels"]
+##########----------                    ----------##########
+##########---------- DEFINING FUNCTIONS ----------##########
+##########----------                    ----------##########
+
+##########---------- FOR TASK 1 ----------##########
+
+def read_file(filename: str):
+    """
+    read .bin file
+    """
+    with open(filename, 'rb') as f:
+        magic, size = struct.unpack(">II", f.read(8))
+        if "images" in filename:
+            nrows, ncols = struct.unpack(">II", f.read(8))
+            data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+            data = data.reshape((size, nrows, ncols))
+        elif "labels" in filename:
+            data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+            data = data.reshape((size,))
+        return data
+
+##########----------                    ----------##########
+##########----------   RUNNING TASK 1   ----------##########
+##########----------                    ----------##########
+
+filenames = ["train_images.bin", "train_labels.bin", "test_images.bin", "test_labels.bin"]
 filepaths = []
-for i in range(4):
-    filepaths.append(os.path.join(os.getcwd(), "Classification MNIST\MNist_ttt4275", filenames[i]))
+for name in filenames:
+    filepaths.append(os.path.join(os.getcwd(), "Classification MNIST\MNist_ttt4275", name))
 
-with open(filepaths[0],'rb') as f:
-    magic, size = struct.unpack(">II", f.read(8))
-    nrows, ncols = struct.unpack(">II", f.read(8))
-    data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
-    data = data.reshape((size, nrows, ncols))
-
-with open(filepaths[1],'rb') as f:
-    magic, size = struct.unpack(">II", f.read(8))
-    data_num = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
-    data_num = data_num.reshape((size,)) # (Optional)
+train_images = read_file(filepaths[0])
+train_labels = read_file(filepaths[1])
+test_images  = read_file(filepaths[2])
+test_labels  = read_file(filepaths[3])
 
 for i in range(100):
-    plt.imshow(data[i,:,:], cmap='gray')
-    print(data_num[i])
+    plt.imshow(train_images[i,:,:])
+    print(train_labels[i])
     plt.show()
