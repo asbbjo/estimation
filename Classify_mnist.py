56611fce61e1bbb6e8bc091c4fd661b6f1c14de4
vebroo <vebjorn.rooth@gmail.com>
2023-04-27 12:17:12 +0200

    added attempt at task 1

--- Classify_mnist.py
+++ Classify_mnist.py
@@ -26,6 +26,19 @@ def read_file(filename: str):
             data = data.reshape((size,))
         return data
 
+def error_rate(confusion_matrix):
+    """
+    calculate error rate
+    """
+    # total number of predictions
+    total_predictions = np.sum(confusion_matrix)
+    # number of incorrect predictions
+    incorrect_predictions = total_predictions - np.trace(confusion_matrix)
+
+    error_rate = incorrect_predictions / total_predictions
+    error_rate = round(error_rate, 3)
+    return error_rate
+
 ##########----------                    ----------##########
 ##########----------   RUNNING TASK 1   ----------##########
 ##########----------                    ----------##########
@@ -40,7 +53,26 @@ train_labels = read_file(filepaths[1])
 test_images  = read_file(filepaths[2])
 test_labels  = read_file(filepaths[3])
 
-for i in range(100):
-    plt.imshow(train_images[i,:,:])
-    print(train_labels[i])
-    plt.show()
+# for i in range(100):
+#     plt.imshow(train_images[i,:,:])
+#     print(train_labels[i])
+#     plt.show()
+
+nrows = 1000
+ncols = 6000
+
+distances = np.zeros((nrows, ncols))
+
+for uno in range(nrows):
+    for dos in range(ncols):
+        distances[uno, dos] = np.linalg.norm(test_images[uno] - train_images[dos])
+
+
+predictions = np.array([train_labels[np.argmin(distances[i])] for i in range(nrows)])
+
+print(predictions)
+print(test_labels[:nrows])
+
+cm = confusion_matrix(test_labels[:nrows], predictions)
+error = error_rate(cm)
+print(cm, error)
